8c693c0505190f4cf7e832e4bb039342a767fea2
Sam <sygamerz@gmail.com>
2024-02-18 20:57:59 +0100

    Swap tuples for plain ints in hex form

--- world/textures.py
+++ world/textures.py
@@ -3,16 +3,16 @@ from enum import Enum
 
 
 class Textures(Enum):
-    VOID = (0, 0)
-    GRASS = (1, 0)
-    PLAINS = (2, 0)
-    DIRT = (3, 0)
-    COBBLESTONE = (4, 0)
-    SAND = (5, 0)
-    COBBLESTONE_STAIRS = (6, 0)
-    FIRESTONE = (7, 0)
-    WATER = (0, 1)
-    LAVA = (1, 1)
+    VOID = 0x0
+    GRASS = 0x1
+    PLAINS = 0x2
+    DIRT = 0x3
+    COBBLESTONE = 0x4
+    SAND = 0x5
+    COBBLESTONE_STAIRS = 0x6
+    FIRESTONE = 0x7
+    WATER = 0x8
+    LAVA = 0x9
 
 class Tiles(Enum):
     BREAKABLE = [Textures.COBBLESTONE]
