d852ac8584d1bc5badebc0763ac2f77bfe2d2bfe
Sam <sygamerz@gmail.com>
2024-02-18 20:29:08 +0100

    Remove independant update loop function from loading screen

--- gui/screens/loading_screen.py
+++ gui/screens/loading_screen.py
@@ -31,21 +31,6 @@ class LoadingScreen(Screen):
         self.progress_bar.update(self.window)
         self.progress_bar.center(0, 0, self.window.width, self.window.height)
 
-    def independant_update(self, thread_event, game, title):
-        self.progress_bar.set_title(title)
-        game.update_ui()
-        while self.progress_bar.get_value() < self.progress_bar.get_value_bounds()[1]:
-            if thread_event.is_set():
-                break
-            game.clear((140, 150, 235))
-            for e in event.get():
-                if e.type == VIDEORESIZE:
-                    game.resize(e)
-                    game.update_ui()
-            game.screens.update()
-            game.draw_fps()
-            game.tick()
-
     def set_state(self, state):
         super().set_state(state)
         self.progress_bar.set_state(state)
