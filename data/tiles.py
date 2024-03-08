f129fe33a1e049818896e664a793ab70d2d340b9
SammygoodTunes <sygamerz@gmail.com>
2024-03-08 13:27:35 +0100

    Add damage and damage delay properties

--- data/tiles.py
+++ data/tiles.py
@@ -6,8 +6,10 @@ class Tile:
     DEFAULT_ATLAS: str = "assets/atlas.png"
 
     def __init__(self, x, y):
-        self._xy = (x, y)
+        self._xy: tuple = (x, y)
         self._resistance: int = 0
+        self._damage: int = 0
+        self._damage_delay: float = 0.0
 
     def set_xy(self, x, y):
         self._xy = (x, y)
@@ -23,6 +25,20 @@ class Tile:
     def get_resistance(self):
         return self._resistance
 
+    def set_damage(self, damage: int):
+        self._damage = damage
+        return self
+
+    def get_damage(self):
+        return self._damage
+
+    def set_damage_delay(self, damage_delay: float):
+        self._damage_delay = damage_delay
+        return self
+
+    def get_damage_delay(self):
+        return self._damage_delay
+
 class Tiles:
     VOID = Tile(0, 0)
     GRASS = Tile(1, 0)
@@ -33,8 +49,9 @@ class Tiles:
     COBBLESTONE_STAIRS = Tile(6, 0)
     FIRESTONE = Tile(7, 0)
     WATER = Tile(0, 1)
-    LAVA = Tile(1, 1)
+    LAVA = Tile(1, 1).set_damage(3).set_damage_delay(0.5)
     BREAK_TILES_ANIM = [Tile(x, 7) for x in range(8)]
 
 class TileTypes(Enum):
     BREAKABLE = [Tiles.COBBLESTONE]
+    LETHAL = [Tiles.LAVA]
