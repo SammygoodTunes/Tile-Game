122b698e3464fffd82251b1c6665067013850434
SammygoodTunes <sygamerz@gmail.com>
2024-03-09 18:37:32 +0100

    Fix tests for assets

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -3,21 +3,23 @@
 Tests dedicated to the assets.
 '''
 
-from os.path import exists, isfile, join
+from os.path import isfile
+from importlib import resources as impr
+from game import assets
 
-ASSETS_DIR = 'assets'
+ASSETS_DIR = impr.files(assets) 
 
 def test_assets_folder_exists():
-	assert exists(ASSETS_DIR)
+    assert ASSETS_DIR.is_dir()
 
 def test_atlas_asset_exists():
-	assert exists(join(ASSETS_DIR, 'atlas.png'))
+    assert isfile(ASSETS_DIR / 'atlas.png')
 
 def test_font_asset_exists():
-	assert exists(join(ASSETS_DIR, 'font.ttf'))
+    assert isfile(ASSETS_DIR / 'font.ttf')
 
 def test_items_asset_exists():
-	assert exists(join(ASSETS_DIR, 'items.png'))
+    assert isfile(ASSETS_DIR / 'items.png')
 
 def test_player_asset_exist():
-	assert exists(join(ASSETS_DIR, 'player.png'))
+    assert isfile(ASSETS_DIR / 'player.png')
