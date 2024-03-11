55df6f2e59b38efdd3fcac46d8842809d5eec4e6
Sam <sygamerz@gmail.com>
2024-03-11 15:06:44 +0100

    Format tests for assets

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -3,23 +3,29 @@
 Tests dedicated to the assets.
 '''
 
-from os.path import isfile
 from importlib import resources as impr
+from os.path import isfile
+
 from game import assets
 
 ASSETS_DIR = impr.files(assets) 
 
+
 def test_assets_folder_exists():
     assert ASSETS_DIR.is_dir()
+
 
 def test_atlas_asset_exists():
     assert isfile(ASSETS_DIR / 'atlas.png')
 
+
 def test_font_asset_exists():
     assert isfile(ASSETS_DIR / 'font.ttf')
 
+
 def test_items_asset_exists():
     assert isfile(ASSETS_DIR / 'items.png')
+
 
 def test_player_asset_exist():
     assert isfile(ASSETS_DIR / 'player.png')
