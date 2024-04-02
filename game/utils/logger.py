7719ed2043bdb999f49805bd834e94a20ef38ce9
SammygoodTunes <sygamerz@gmail.com>
2024-04-02 10:58:32 +0200

    Ignore unknown arguments

--- game/utils/logger.py
+++ game/utils/logger.py
@@ -9,7 +9,7 @@ import logging
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--debug', action=argparse.BooleanOptionalAction,
                     default=False, type=bool, help='enable debug info')
-args = parser.parse_args()
+args, unknown_args = parser.parse_known_args()
 
 _format = '\t%(levelname)s(%(name)s) - %(asctime)s\t%(message)s'
 logging.basicConfig(level=logging.DEBUG, format=_format) if args.debug else logging.basicConfig(level=logging.INFO, format=_format)
