436c165bebe321b46ae1267995244d21db6ec734
SammygoodTunes <sygamerz@gmail.com>
2024-04-03 16:15:33 +0200

    Add placeable tile type

--- game/data/tiles.py
+++ game/data/tiles.py
@@ -108,5 +108,7 @@ class TileTypes:
     Class for regrouping tiles into different categories.
     """
 
+    PLACEABLE: tuple[Tiles] = (Tiles.GRASS, Tiles.PLAINS, Tiles.DIRT, Tiles.COBBLESTONE, Tiles.SAND,
+                               Tiles.COBBLESTONE_STAIRS, Tiles.FIRESTONE, Tiles.WATER, Tiles.LAVA)
     BREAKABLE: tuple[Tiles] = (Tiles.COBBLESTONE,)
     LETHAL: tuple[Tiles] = (Tiles.LAVA,)
