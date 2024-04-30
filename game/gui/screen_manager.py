16a17475e5b61bec9c29a123a9edf58538408fa0
Sam <sygamerz@gmail.com>
2024-04-30 15:37:16 +0200

    Add server menu screen functionality

--- game/gui/screen_manager.py
+++ game/gui/screen_manager.py
@@ -5,7 +5,8 @@ from game.data.mouse_properties import Mouse
 from game.gui.screens.credits_screen import CreditsScreen
 from game.gui.screens.gameover_screen import GameoverScreen
 from game.gui.screens.loading_screen import LoadingScreen
-from game.gui.screens.mainmenu_screen import MainMenu
+from game.gui.screens.mainmenu_screen import MainMenuScreen
+from game.gui.screens.servermenu_screen import ServerMenuScreen
 from game.gui.screens.options_screen import OptionsScreen
 from game.gui.screens.pause_screen import PauseScreen
 from game.utils.logger import logger
@@ -20,7 +21,8 @@ class Screens:
         self.window = window
         self.game = None
         self.screen = self.window.screen
-        self.main_menu = MainMenu(window)
+        self.main_menu_screen = MainMenuScreen(window)
+        self.server_menu_screen = ServerMenuScreen(window)
         self.loading_screen = LoadingScreen(window)
         self.pause_screen = PauseScreen(window)
         self.options_screen = OptionsScreen(window)
@@ -59,24 +61,29 @@ class Screens:
                     if self.game.start_game:
                         self.pause_screen.set_state(True)
                     else:
-                        self.main_menu.set_state(True)
+                        self.main_menu_screen.set_state(True)
 
                 elif self.credits_screen.back_button.is_hovering_over():
                     self.credits_screen.set_state(False)
-                    self.main_menu.set_state(True)
+                    self.main_menu_screen.set_state(True)
 
-                elif self.main_menu.play_button.is_hovering_over() and self.main_menu.get_state():
-                    self.window.start_game = True
-                    self.main_menu.set_state(False)
-                    self.game.world.initialise(self.game)
-                elif self.main_menu.options_button.is_hovering_over() and self.main_menu.get_state():
+                elif self.main_menu_screen.play_button.is_hovering_over() and self.main_menu_screen.get_state():
+                    # self.window.start_game = True
+                    self.main_menu_screen.set_state(False)
+                    self.server_menu_screen.set_state(True)
+                    # self.game.world.initialise(self.game)
+                elif self.main_menu_screen.options_button.is_hovering_over() and self.main_menu_screen.get_state():
                     self.options_screen.set_state(True)
-                    self.main_menu.set_state(False)
-                elif self.main_menu.credits_button.is_hovering_over() and self.main_menu.get_state():
+                    self.main_menu_screen.set_state(False)
+                elif self.main_menu_screen.credits_button.is_hovering_over() and self.main_menu_screen.get_state():
                     self.credits_screen.set_state(True)
-                    self.main_menu.set_state(False)
-                elif self.main_menu.quit_button.is_hovering_over() and self.main_menu.get_state():
+                    self.main_menu_screen.set_state(False)
+                elif self.main_menu_screen.quit_button.is_hovering_over() and self.main_menu_screen.get_state():
                     self.window.stop()
+
+                elif self.server_menu_screen.back_button.is_hovering_over() and self.server_menu_screen.get_state():
+                    self.server_menu_screen.set_state(False)
+                    self.main_menu_screen.set_state(True)
 
                 elif self.pause_screen.resume_button.is_hovering_over():
                     self.pause_screen.set_state(False)
@@ -88,7 +95,7 @@ class Screens:
                     self.window.start_game = False
                     self.window.paused = False
                     self.pause_screen.set_state(False)
-                    self.main_menu.set_state(True)
+                    self.main_menu_screen.set_state(True)
 
                 elif self.gameover_screen.regen_button.is_hovering_over():
                     self.gameover_screen.set_state(False)
@@ -98,13 +105,14 @@ class Screens:
                     self.gameover_screen.set_state(False)
                     self.window.start_game = False
                     self.window.paused = False
-                    self.main_menu.set_state(True)
+                    self.main_menu_screen.set_state(True)
 
     def update(self) -> None:
         """
         Update the screens.
         """
-        self.main_menu.draw()
+        self.main_menu_screen.draw()
+        self.server_menu_screen.draw()
         self.loading_screen.draw()
         self.pause_screen.draw()
         self.options_screen.draw()
@@ -115,7 +123,8 @@ class Screens:
         """
         Update the screen UIs.
         """
-        self.main_menu.update_ui()
+        self.main_menu_screen.update_ui()
+        self.server_menu_screen.update_ui()
         self.loading_screen.update_ui()
         self.pause_screen.update_ui()
         self.options_screen.update_ui()
