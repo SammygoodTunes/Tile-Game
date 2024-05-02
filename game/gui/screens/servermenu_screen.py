21c59d7e69c25722cdd36ea8bb0bc88e96f8e455
SammygoodTunes <sygamerz@gmail.com>
2024-05-02 20:08:50 +0200

    Edit server menu button layout

--- game/gui/screens/servermenu_screen.py
+++ game/gui/screens/servermenu_screen.py
@@ -18,8 +18,10 @@ class ServerMenuScreen(Screen):
 		self.window = window
 		self.faded_surface = self.initialise_surface()
 		self.subtitle_label = Label("Join a server or create one yourself for people to join.").set_font_sizes((7, 8, 10)).set_colour((215, 215, 215))
-		self.join_button = Button("Join server")
-		self.create_button = Button("Create server")
+		self.join_button = Button("Join server").set_background_colour((150, 160, 255))
+		self.join_button.label.set_colour((190, 200, 255))
+		self.create_button = Button("Create server").set_background_colour((150, 255, 160))
+		self.create_button.label.set_colour((190, 255, 200))
 		self.back_button = Button("Back")
 		logger.debug(f'Created {__class__.__name__} with attributes {self.__dict__}')
 
@@ -51,9 +53,9 @@ class ServerMenuScreen(Screen):
 		self.subtitle_label.update(self.window)
 		self.subtitle_label.center_with_offset(0, 0, self.window.width, self.window.height, 0, -self.join_button.get_height() - self.subtitle_label.get_total_height() * 3)
 		self.join_button.update(self.window)
-		self.join_button.center_with_offset(0, 0, self.window.width, self.window.height, 0, -self.join_button.get_height())
+		self.join_button.center_with_offset(0, 0, self.window.width, self.window.height, int(-self.create_button.get_width() // 1.5), -self.join_button.get_height())
 		self.create_button.update(self.window)
-		self.create_button.center_with_offset(0, 0, self.window.width, self.window.height, 0, 5)
+		self.create_button.center_with_offset(0, 0, self.window.width, self.window.height, int(self.join_button.get_width() // 1.5), -self.join_button.get_height())
 		self.back_button.update(self.window)
 		self.back_button.center_with_offset(0, 0, self.window.width, self.window.height, 0, self.join_button.get_height() + self.create_button.get_height() + 10)
 
