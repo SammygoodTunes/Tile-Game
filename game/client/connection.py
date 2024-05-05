dc523a629f5cb7eb192a08345afd33d43975f0b4
SammygoodTunes <sygamerz@gmail.com>
2024-05-06 00:04:16 +0200

    Task names and proper server client disconnection

--- game/client/connection.py
+++ game/client/connection.py
@@ -1,10 +1,19 @@
 
 import socket
 from threading import Thread
+from time import time, sleep
 
 from game.utils.logger import logger
 from game.network.protocol import Protocol
 from game.network.packet import Hasher, Compressor
+
+
+class Tasks:
+    """
+    Class for defining the connection tasks.
+    """
+
+    CONNECT, UPDATE = range(0, 2)
 
 
 class Connection:
@@ -12,34 +21,39 @@ class Connection:
     Class for creating a server connection.
     """
 
-    PENDING, GETDATA = range(-1, -3, -1)
-    SUCCESS, INVALID, REFUSED, TIMEOUT, NOROUTE = range(0, 5)
+    IDLE, PENDING, GETDATA = range(-1, -4, -1)
+    SUCCESS, INVALID, REFUSED, TIMEOUT, NOROUTE, DISCONNECTED, ERROR = range(0, 7)
 
     def __init__(self, host: str, port: int):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(5.0)
         self.host = host
         self.port = port
-        self.state = None
+        self.state = Connection.IDLE
+        self.data: any = None
         logger.debug(f'Created {__class__.__name__} with attributes {self.__dict__}')
 
-    def connect(self):
+    def connect(self) -> None:
+        """
+        Attempt to connect to a server with the provided host and port.
+        Any errors or failures will raise specific exceptions.
+        """
         try:
             self.state = Connection.PENDING
             self.sock.connect((socket.gethostbyname(socket.gethostname()) if self.host.lower() == 'localhost' else self.host, self.port))
             self.sock.send(Hasher.enhash(Protocol.RECOGNITION_CMD_REQ))
             data = self.sock.recv(Protocol.BUFFER_SIZE).decode(Protocol.ENCODING)
             if data and data == Hasher.hash(Protocol.RECOGNITION_CMD_RES):
-                self.sock.send(Hasher.enhash(Protocol.MAPDATA_CMD_REQ))
+                self.sock.send(Hasher.enhash(Protocol.MAPOBJ_CMD_REQ))
             data = self.sock.recv(Protocol.BUFFER_SIZE).decode(Protocol.ENCODING)
-            if data and data == Hasher.hash(Protocol.MAPDATA_CMD_RES):
+            if data and data == Hasher.hash(Protocol.MAPOBJ_CMD_RES):
                 self.state = Connection.GETDATA
-                compressed_map_data = b''
+                compressed_map_obj = b''
                 data = self.sock.recv(Protocol.BUFFER_SIZE)
                 while data != Hasher.enhash(Protocol.MAPREADY_CMD):
-                    compressed_map_data += data
+                    compressed_map_obj += data
                     data = self.sock.recv(Protocol.BUFFER_SIZE)
-                map_data = Compressor.decompress(compressed_map_data.strip())
+                self.data = Compressor.decompress(compressed_map_obj.strip())
                 self.state = Connection.SUCCESS
                 return
             self.state = Connection.REFUSED
@@ -51,8 +65,34 @@ class Connection:
             self.state = Connection.INVALID
         except OSError:
             self.state = Connection.NOROUTE
+        except Exception:
+            self.state = Connection.ERROR
 
-    def start(self):
-        connection_thread = Thread(target=self.connect)
+    def disconnect(self) -> None:
+        """
+        Attempt to disconnect from the server.
+        Any errors or failures will raise specific exceptions.
+        """
+        if self.state == Connection.SUCCESS:
+            self.sock.send(Hasher.enhash(Protocol.DISCONNECT_CMD))
+            self.state = Connection.IDLE
+            self.sock.close()
+
+    def update(self):
+        """
+        Update and verify the connection every so often.
+        """
+        while self.state <= 0 and self.state != Connection.IDLE:
+            try:
+                self.sock.send(Hasher.enhash(Protocol.VERIFY_CMD))
+                sleep(5)
+            except BrokenPipeError:
+                self.state = Connection.DISCONNECTED
+
+    def start(self, task: Tasks):
+        connection_thread: Thread = Thread()
+        if task == Tasks.CONNECT:
+            connection_thread = Thread(target=self.connect)
+        elif task == Tasks.UPDATE:
+            connection_thread = Thread(target=self.update)
         connection_thread.start()
-
