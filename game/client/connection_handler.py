23b590030659e2116ae13d104f68b056d757074a
SammygoodTunes <sygamerz@gmail.com>
2024-05-06 00:06:12 +0200

    Use connection tasks and properly close connection when asked to

--- game/client/connection_handler.py
+++ game/client/connection_handler.py
@@ -1,7 +1,5 @@
 
-from game.client.connection import Connection
-from game.gui.screens.serverjoin_screen import ServerJoinScreen
-from game.gui.screens.serverconnect_screen import ServerConnectScreen
+from game.client.connection import Connection, Tasks
 
 
 class ConnectionHandler:
@@ -11,17 +9,39 @@ class ConnectionHandler:
 
     def __init__(self):
         self.start_connection = False
+        self.stop_connection = False
         self.connection: Connection | None = None
 
-    def update(self, join_screen: ServerJoinScreen, connection_screen: ServerConnectScreen):
+    def update(self, game):
         if self.start_connection:
             self.start_connection = False
-            self.connection = Connection(join_screen.ip_input.get_text(), int(join_screen.port_input.get_text()))
-            self.connection.start()
+            self.connection = Connection(
+                game.screens.server_join_screen.ip_input.get_text(),
+                int(game.screens.server_join_screen.port_input.get_text()))
+            self.connection.start(task=Tasks.CONNECT)
 
-        if self.connection:
-            connection_state = self.connection.state
-            connection_screen.update_info_label(connection_state)
-            if connection_state >= 0:
+        if self.stop_connection:
+            self.stop_connection = False
+            self.connection.disconnect()
+            self.connection = None
+
+        if not self.connection:
+            return
+
+        connection_state = self.connection.state
+        if not game.start_game:
+            game.screens.server_connect_screen.update_info_label(connection_state)
+            if connection_state > 0:
                 self.connection = None
+            if connection_state == Connection.SUCCESS:
+                game.world.set_map(self.connection.data)
+                game.world.initialise()
+                game.world.get_map().load()
+                game.player.set_ideal_spawnpoint(game.world.get_map(), game.camera)
+                game.start_game = True
+                game.screens.server_connect_screen.set_state(False)
+                self.connection.start(task=Tasks.UPDATE)
 
+        if connection_state > 0 and game.start_game:
+            game.start_game = False
+            game.screens.server_connect_screen.set_state(True)
