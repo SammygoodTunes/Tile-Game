a8d51a122939ee729b4577ca31d6093011973d7f
SammygoodTunes <sygamerz@gmail.com>
2024-05-14 09:15:51 +0200

    Add and refactor to use new connection states

--- game/client/connection.py
+++ game/client/connection.py
@@ -3,6 +3,7 @@ import socket
 from threading import Thread
 from time import sleep
 
+from game.data.states import ConnectionStates
 from game.utils.logger import logger
 from game.network.protocol import Protocol
 from game.network.packet import Hasher, Compressor
@@ -21,15 +22,12 @@ class Connection:
     Class for creating a server connection.
     """
 
-    IDLE, PENDING, GETDATA = range(-1, -4, -1)
-    SUCCESS, INVALID, REFUSED, TIMEOUT, NOROUTE, DISCONNECTED, ERROR = range(0, 7)
-
     def __init__(self, host: str, port: int):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(5.0)
         self.host = host
         self.port = port
-        self.state = Connection.IDLE
+        self.state = ConnectionStates.IDLE
         self.data: any = None
         logger.debug(f'Created {__class__.__name__} with attributes {self.__dict__}')
 
@@ -39,7 +37,7 @@ class Connection:
         Any errors or failures will raise specific exceptions.
         """
         try:
-            self.state = Connection.PENDING
+            self.state = ConnectionStates.PENDING
             self.sock.connect((socket.gethostbyname(socket.gethostname()) if self.host.lower() == 'localhost' else self.host, self.port))
             self.sock.send(Hasher.enhash(Protocol.RECOGNITION_CMD_REQ))
             self.sock.send(Hasher.enhash(Protocol.RECOGNITION_CMD_REQ))
@@ -48,47 +46,47 @@ class Connection:
                 self.sock.send(Hasher.enhash(Protocol.MAPOBJ_CMD_REQ))
             data = self.sock.recv(Protocol.BUFFER_SIZE).decode(Protocol.ENCODING)
             if data and data == Hasher.hash(Protocol.MAPOBJ_CMD_RES):
-                self.state = Connection.GETDATA
+                self.state = ConnectionStates.GETDATA
                 compressed_map_obj = b''
                 data = self.sock.recv(Protocol.BUFFER_SIZE)
                 while data != Hasher.enhash(Protocol.MAPREADY_CMD):
                     compressed_map_obj += data
                     data = self.sock.recv(Protocol.BUFFER_SIZE)
                 self.data = Compressor.decompress(compressed_map_obj.strip())
-                self.state = Connection.SUCCESS
+                self.state = ConnectionStates.SUCCESS
                 return
-            self.state = Connection.REFUSED
+            self.state = ConnectionStates.REFUSED
         except ConnectionRefusedError:
-            self.state = Connection.REFUSED
+            self.state = ConnectionStates.REFUSED
         except TimeoutError:
-            self.state = Connection.TIMEOUT
+            self.state = ConnectionStates.TIMEOUT
         except socket.gaierror:
-            self.state = Connection.INVALID
+            self.state = ConnectionStates.INVALID
         except OSError:
-            self.state = Connection.NOROUTE
+            self.state = ConnectionStates.NOROUTE
         except Exception:
-            self.state = Connection.ERROR
+            self.state = ConnectionStates.ERROR
 
     def disconnect(self) -> None:
         """
         Attempt to disconnect from the server.
         Any errors or failures will raise specific exceptions.
         """
-        if self.state == Connection.SUCCESS:
+        if self.state == ConnectionStates.SUCCESS:
             self.sock.send(Hasher.enhash(Protocol.DISCONNECT_CMD))
-            self.state = Connection.IDLE
+            self.state = ConnectionStates.IDLE
             self.sock.close()
 
     def update(self):
         """
         Update and verify the connection every so often.
         """
-        while self.state <= 0 and self.state != Connection.IDLE:
+        while self.state <= 0 and self.state != ConnectionStates.IDLE:
             try:
                 self.sock.send(Hasher.enhash(Protocol.VERIFY_CMD))
                 sleep(5)
             except BrokenPipeError:
-                self.state = Connection.DISCONNECTED
+                self.state = ConnectionStates.DISCONNECTED
 
     def start(self, task: Tasks):
         connection_thread: Thread = Thread()
--- game/data/states.py
+++ game/data/states.py
@@ -1,5 +1,15 @@
 
 class MapStates:
-
+    """
+    Map states.
+    """
     GENMAP = 'GEN-MAP'
     READY = 'READY'
+
+
+class ConnectionStates:
+    """
+    Connection states.
+    """
+    IDLE, PENDING, GETDATA = range(-1, -4, -1)
+    SUCCESS, INVALID, REFUSED, TIMEOUT, NOROUTE, DISCONNECTED, ERROR = range(0, 7)
