24b8a41748ef5993ae02752fc87e4017ad2327db
SammygoodTunes <sygamerz@gmail.com>
2024-05-16 21:17:15 +0200

    Make address re-usable

--- game/server/server.py
+++ game/server/server.py
@@ -67,6 +67,7 @@ class Server:
         """
         self.state = ServerStates.STARTING
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.world_handler = WorldHandler()
 
         '''logging.basicConfig(
