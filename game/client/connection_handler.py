f069b2447c49d160ab4b68ee6e58685a5e0442bc
SammygoodTunes <sygamerz@gmail.com>
2024-10-12 19:54:00 +0200

    Fix decompression for map dimensions

--- game/client/connection_handler.py
+++ game/client/connection_handler.py
@@ -55,14 +55,16 @@ class ConnectionHandler:
             if connection_state > 0:
                 self.connection = None
             if connection_state == ConnectionStates.SUCCESS:
+                height_pos = MapStructure.MAP_WIDTH_BYTE_SIZE + MapStructure.MAP_HEIGHT_BYTE_SIZE
                 width, height = (
-                    self.connection.data[MapStructure.MAP_WIDTH_BYTE_SIZE - 1],
-                    self.connection.data[MapStructure.MAP_HEIGHT_BYTE_SIZE * 2 - 1]
+                    int.from_bytes(self.connection.data[:MapStructure.MAP_WIDTH_BYTE_SIZE]),
+                    int.from_bytes(self.connection.data[MapStructure.MAP_WIDTH_BYTE_SIZE:height_pos])
                 )
+                print(self.connection.data[:10])
                 tile_data_pos = width * height * TileStructure.TILE_BYTE_SIZE * 2
                 game.client.world.set_map(Map(width, height))
                 game.client.world.get_map().set_tile_data(
-                    self.connection.data[MapStructure.MAP_WIDTH_BYTE_SIZE + MapStructure.MAP_HEIGHT_BYTE_SIZE:tile_data_pos]
+                    self.connection.data[height_pos:tile_data_pos]
                 )
                 game.client.world.get_map().set_dynatile_data(self.connection.data[tile_data_pos + 1:])
                 game.client.world.initialise()
