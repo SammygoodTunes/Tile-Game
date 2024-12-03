d69c6b1a49a4b81321cee945af5bff25da7c6a9c
SammygoodTunes <sygamerz@gmail.com>
2024-12-03 20:04:36 +0100

    Add sphinx config for docs

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,42 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath('../..'))
+
 project = 'Tile-Game'
 author = 'SammygoodTunes'
+copyright = '%Y, SammygoodTunes'
 
-sys.path.insert(0, os.path.abspath('../game'))
+version = '0.0.2a'
+release = '0.0.2a'
+
+language = 'en'
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+pygments_style = 'sphinx'
+
+todo_include_todos = True
+
+autodoc_default_options = {
+    'members': True,
+    'inherited-members': True,
+    'show-inheritance': True,
+}
+
+htmlhelp_basename = 'tilegamedocs'
+
+source_suffix = '.rst'
+
+
